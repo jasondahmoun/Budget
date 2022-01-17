67a387c4abadd1489fc8c9c6b567e52d4a1e2df4
jason <dahmoun.jason@gmail.com>
2022-01-17 14:11:34 +0100

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Jason Dahmoun on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
